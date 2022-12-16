4c24b592989513297e208e8b0f2e122baf2ad33b
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-15 22:01:59 -0500

    Added MathMethods

---
+++ MathMethods/MathMethods/MyMath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    class MyMath
+    {
+
+
+        public int myAdd(int myNum)
+        {
+            myNum = myNum + 5;
+
+        return myNum;
+        }
+
+        public int mySub(int myNum)
+        {
+            myNum = myNum - 2;
+
+            return myNum;
+        }
+
+        public int myMultiply(int myNum)
+        {
+            myNum = myNum * 3;
+
+            return myNum;
+        }
+
+    }
+
+    
+}
---
+++ MathMethods/MathMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, let's math it up.  Please enter a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            MyMath math = new MyMath();
+
+
+            Console.WriteLine("Let's add five to your number, " + userNum);
+            userNum = math.myAdd(userNum);
+            Console.WriteLine("Now the number is " + userNum);
+
+
+            Console.WriteLine("Let's subtract 2 to that new number, " + userNum);
+            userNum = math.mySub(userNum);
+            Console.WriteLine("Now the number is " + userNum);
+
+            Console.WriteLine("Let's multiply 3 to that new number, " + userNum);
+            userNum = math.myMultiply(userNum);
+            Console.WriteLine("Now the number is " + userNum);
+
+            Console.WriteLine("Isn't math cool?");
+            Console.ReadLine();
+
+        }
+    }
+}
