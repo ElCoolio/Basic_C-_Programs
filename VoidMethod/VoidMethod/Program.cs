796fca2b281be03d1f081656b0f8696da511a86f
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-16 12:43:36 -0500

    Made VoidMethod

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMethod myVoid = new VoidMethod();
+            int x = 5;
+            int y = 7;
+
+            // using a void method we will post int y to the screen.  There is a math operation that happens to x, but it isn't returned.
+            Console.WriteLine("We will be using a void method to send some content to the console");
+            VoidMethod.myMethod(x, y);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/VoidMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace VoidMethod
+{
+    class VoidMethod
+    {
+        public static void myMethod(int x, int y)
+        {
+            x = x + 5;
+            Console.WriteLine(y.ToString());
+        }
+
+
+    }
+}
