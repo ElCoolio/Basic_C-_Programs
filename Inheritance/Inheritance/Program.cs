9ae367a6c8c185fc2736877b2bf9591ae4c6765f
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-19 13:56:07 -0500

    Created Inheritance project.

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates the employee object, and give the values that it inherited from the Person Class
+            Employee test = new Employee() { FirstName = "Sample" , LastName = "Student" };
+
+
+            // Uses the superclass method that it inherited from the Person class
+            test.SayName();
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
