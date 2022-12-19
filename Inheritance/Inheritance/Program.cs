29e6fda476df3f0b13561aa3cf64af6b3b9bd8cd
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-19 17:14:11 -0500

    Fixed code

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -12,13 +12,20 @@ namespace Inheritance
         {
             // Creates the employee object, and give the values that it inherited from the Person Class
             Employee test = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1 };
-
+            
+            // This uses polymorphism to create an iQuittable object
+            iQuittable test2 = new Employee() { FirstName = "Sample", LastName = "Employee", Id = 2 };
 
             // Uses the superclass method that it inherited from the Person class
             test.SayName();
 
             // uses the interface class
             test.Quit(test);
+
+
+            //uses the methods inherited on the iQuittable object
+
+            test2.Quit((Employee)test2);
 
             Console.ReadLine();
 
