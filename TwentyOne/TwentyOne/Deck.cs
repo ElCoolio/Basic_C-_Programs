61be4cf29cc92e8d07a64da129e9d4a98fcdde7e
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-19 12:10:58 -0500

    Fixed Void Method

--- TwentyOne/TwentyOne/Deck.cs
+++ TwentyOne/TwentyOne/Deck.cs
@@ -28,12 +28,28 @@ namespace TwentyOne
                     Cards.Add(card);
                 }
             }
-
         }
-
 
         public List<Card> Cards { get; set; }
 
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+
+                    int randomIndex = random.Next(0, this.Cards.Count);
+                    TempList.Add(this.Cards[randomIndex]);
+                    this.Cards.RemoveAt(randomIndex);
+                }
+                this.Cards = TempList;
+            }
+
+        }
 
     }
 }
--- VoidMethod/VoidMethod/Program.cs
+++ VoidMethod/VoidMethod/Program.cs
@@ -14,6 +14,10 @@ namespace VoidMethod
             // using a void method we will post int y to the screen.  There is a math operation that happens to x, but it isn't returned.
             Console.WriteLine("We will be using a void method to send some content to the console");
             VoidMethod.myMethod(x, y);
+
+            // now we are going to call the method specifying the parameters by name, by doing this, the order does not matter.
+            VoidMethod.myMethod(y: 7, x: 5);
+
             Console.ReadLine();
 
         }
