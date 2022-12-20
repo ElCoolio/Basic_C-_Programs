1cfa0647c8adc886708b49f0db8aeef6be53c87f
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-19 19:10:39 -0500

    Completed Enum Project

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("enter the day of the week");
+                string dayinput = Console.ReadLine();
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), dayinput);
+                Console.WriteLine("The value of that day of the week is " + Convert.ToInt32(day));
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+       public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+ 
+}
