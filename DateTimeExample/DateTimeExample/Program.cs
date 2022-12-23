674f413baf8a3c544f4e07f11d6d46d8bd7a4c52
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-23 15:19:53 -0500

    Completed DateTimeExample

---
+++ DateTimeExample/DateTimeExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Program to pratice using the DateTime object
+
+namespace DateTimeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates a DateTime object that is a time stamp when the user starts the program
+            DateTime now = DateTime.Now;
+            
+            //prints that time
+            Console.WriteLine("The current time is " + now);
+
+            //asks for user input
+            Console.WriteLine("please enter a number.");
+
+            //puts user input into a var
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //takes user input and adds that number to the date times to show when 
+            DateTime later = now.AddHours(userInput);
+
+            Console.WriteLine("In {0} hours it will be {1}", userInput, later);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class TwentyOneGame: Game, IWalkAway
+    {
+        public TwentyOneDealer Dealer { get; set; }
+        
+        public override void Play()
+        {
+            Dealer = new TwentyOneDealer();
+            foreach(Player player in Players)
+            {
+                player.Hand = new List<Card>();
+                player.Stay = false;
+            }
+            Dealer.Hand = new List<Card>();
+            Dealer.Stay = false;
+            Dealer.Deck = new Deck();
+            Console.WriteLine("Place your bet!");
+            
+            //For each loop to collect bets
+            foreach(Player player in Players)
+            {
+                int bet = Convert.ToInt32(Console.ReadLine());
+                bool successfullyBet = player.Bet(bet);
+                if (!successfullyBet)
+                {
+                    return;
+                }
+                Bets[player] = bet;
+            }
+
+            //for loop to deal cards
+            for (int i =0; i < 2; i++)
+            {
+                Console.WriteLine("Dealing...");
+                foreach (Player player in Player)
+                {
+                    Console.Write("{0}: ", player.Name);
+                    Dealer.Deal(player.Hand);
+                    // check for blackjack on opening hand
+                    if (i == 1)
+                    {
+
+                    }
+                }
+            }
+
+
+        }
+        public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players:");
+            base.ListPlayers();
+
+        }
+        public void WalkAway(Player player)
+        {
+
+        }
+
+    }
+}
