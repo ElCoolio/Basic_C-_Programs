a843419dc230a15a0470f4a9bb3abaf5c43f33d1
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2022-12-27 11:33:38 -0500

    Made ChainConstructor program

---
+++ ConstVar/ConstVar/ChainConstruct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    public class ChainConstruct
+    {
+        // declare vars
+        private string name;
+        private int number;
+
+        // main constructor, asks for a name and a number
+        public ChainConstruct(string name, int number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+        // chained constructor, only takes in a name, uses original constructor
+        public ChainConstruct(string name) : this(name, 10)
+        {
+            
+        }
+
+        //simple writeline method to show constructor vars
+        public void myScript()
+        {
+            Console.WriteLine("It is nice to meet you, {0}, the magic number is {1}",this.name,this.number);
+        }
+
+        //simple writeline method to declare a const var
+        public void myScript2()
+        {
+            const string script = ("This next line is using the chain constructor that ignores your number input");
+            Console.WriteLine(script);
+        }
+
+    }
+}
---
+++ ConstVar/ConstVar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get input
+            Console.WriteLine("Hello there, please enter your name");
+            // uses var to make the variable, instead of declaring it a string
+            var name = Console.ReadLine();
+            Console.WriteLine("Now enter the magic number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // makes an object using the main constructor
+            ChainConstruct newChain = new ChainConstruct(name, num);
+            newChain.myScript();
+
+            // makes a second object using the chained constructor
+            var newChain2 = new ChainConstruct(name);
+            newChain2.myScript2();
+            newChain2.myScript();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
