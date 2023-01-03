25fded593ef20bbca152c4be13ac59f6ab31ec45
ElCoolio <113545574+ElCoolio@users.noreply.github.com>
2023-01-03 17:42:21 -0500

    Entity Framework Challenge

---
+++ Final/Final/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Final.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private static System.Timers.Timer timer;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString();
+            ViewData["TimeStamp"] = dateTime;
+        }
+
+
+    }
+}
